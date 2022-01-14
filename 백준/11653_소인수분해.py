e80c90a86c0cb26f5991122e814e1ad040367422
oneweek-hi <21600763@handong.edu>
2022-01-14 23:25:08 +0900

    :memo: 백준 문제풀기

---
+++ 백준/11653_소인수분해.py
@@ -0,0 +1,21 @@
+n=int(input())
+i=2
+result=[]
+while i < n+1:
+
+    if n ==1:
+        break
+
+    if n%i == 0:
+        n=n//i
+        result.append(i)
+        continue
+
+    i += 1
+
+
+for i in range(len(result)):
+    print(result[i])
+
+
+
