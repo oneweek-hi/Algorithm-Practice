b4c3cacce781c860addc678eb0dd0917da3237c7
oneweek-hi <21600763@handong.edu>
2022-01-24 23:48:31 +0900

    :memo: 백준 풀기

--- 백준/1009_분산처리.py
+++ 백준/1009_분산처리.py
@@ -1,7 +1,31 @@
+t = int(input())
+for _ in range(t):
+    a,b = map(int,input().split())
+    aa=a%10
 
-n = int(input())
-arr=[list(map(int,input().split())) for _ in range(n)]
+    if aa == 0: 
+        print(10)
+    elif aa in [1,5,6]:
+        print(aa)
+    elif aa in [4,9]:
+        bb=b%2
+        if bb == 0:
+            print(aa*aa%10)
+        else:
+            print(aa)
+    else:
+        bb=b%4
+        if bb ==0:
+            print(aa**4%10)
+        else:
+            print(aa**bb%10)
 
-for i in range(n):
-    a = arr[i][0]**arr[i][1]
-    print(a%10)
+
+#
+# n = int(input())
+#
+# for _ in range(n):
+#     a, b = map(int, input().split())
+#     print((a**b)%10)
+#
+#
